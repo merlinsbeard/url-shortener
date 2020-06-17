ce0234e4c78e9dfa521da8308f68135ef6b4124e
Ben <me@benpaat.xyz>
2020-06-17 13:39:28 +0800

    🌱Initial Commit

---
+++ index.ts
@@ -0,0 +1,136 @@
+// https://dev.to/aligoren/building-url-shortener-with-mongodb-express-framework-and-typescript-4a71
+import cors from "cors";
+import * as dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import { nanoid } from "nanoid";
+import mongoose, { Document, Schema } from "mongoose";
+require("mongoose-type-url");
+
+dotenv.config();
+
+const app: express.Application = express();
+app.use(helmet());
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(express.static("./public"));
+
+const mongoDB: string =
+  process.env.MONGO_URL || "mongodb://127.0.0.1:27017/shorter";
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to mongodb"))
+  .catch(function (err) {
+    console.error(`Could not connect ${err}`);
+  });
+
+interface IURL extends mongoose.Document {
+  longUrl: string;
+  shortUrl: string;
+}
+
+const UrlModel = mongoose.model<IURL>(
+  "URL",
+  new Schema({
+    longUrl: {
+      required: true,
+      type: String,
+      trim: true,
+    },
+    shortUrl: {
+      required: true,
+      type: String,
+      unique: true,
+      trim: true,
+      maxlength: 5,
+    },
+  })
+);
+
+app.get("/hi", (req, res) => res.send("hello 🌊"));
+
+const validURL = (str: string) => {
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" +
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+      "((\\d{1,3}\\.){3}\\d{1,3}))" +
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+      "(\\?[;&a-z\\d%_.~+=-]*)?" +
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  );
+  return !!pattern.test(str);
+};
+
+app.post("/short", async (req, res, next) => {
+  let { longUrl, shortUrl } = req.body;
+
+  console.log(shortUrl);
+
+  try {
+    if (!shortUrl) {
+      shortUrl = nanoid(5);
+    } else {
+      const URL = await UrlModel.findOne({ shortUrl });
+      if (URL) {
+        throw new Error("short url is alreay used");
+      }
+    }
+
+    const isValidUrl = validURL(longUrl);
+    console.log(isValidUrl);
+    if (!isValidUrl) {
+      throw new Error("Invalid URL");
+    }
+
+    const validateURL = new UrlModel({ longUrl, shortUrl });
+    await validateURL.save();
+    return res.send(validateURL);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Redirects to shortUrl url
+app.get("/:id", async (req, res) => {
+  const { id: shortUrl } = req.params;
+  try {
+    const url = await UrlModel.findOne({ shortUrl });
+    if (url) {
+      return res.redirect(url.longUrl);
+    }
+    return res.status(404).send("Not Found");
+  } catch (error) {
+    return res.status(404).send("Not Found");
+  }
+});
+
+app.use(
+  (
+    error: any,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    console.log(error);
+    if (error.status) {
+      res.status(error.status);
+    } else {
+      res.status(400);
+    }
+    console.log(error.stack);
+    res.json({
+      message: error.message,
+      stack: process.env.NODE_ENV === "production" ? "hmm" : error.stack,
+    });
+  }
+);
+
+const PORT: number = parseInt(process.env.PORT as string, 10) || 1337;
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}!`);
+});
---
+++ public/app.js
@@ -0,0 +1,133 @@
+const useInput = (initialValue) => {
+  const [value, setValue] = React.useState(initialValue);
+  return {
+    value,
+    setValue,
+    reset: () => setValue(""),
+    bind: {
+      value,
+      onChange: (event) => {
+        setValue(event.target.value);
+      },
+    },
+  };
+};
+
+function FormSuccess(props) {
+  return (
+    <>
+      <a href={`${window.location.protocol}//${props.url}`}>
+        View link here: {`${window.location.protocol}//${props.url}`}
+      </a>
+    </>
+  );
+}
+
+function FormFail(props) {
+  return <div className="has-text-danger">{props.message}</div>;
+}
+
+function UrlForm(props) {
+  const { value: longUrl, bind: bindLongUrl, reset: resetLongUrl } = useInput(
+    ""
+  );
+  const {
+    value: shortUrl,
+    bind: bindShortUrl,
+    reset: resetShortUrl,
+  } = useInput("");
+  const [status, setStatus] = React.useState("NO_ACTION");
+  const [errorMessage, setErrorMessage] = React.useState();
+  const [orange, setOrange] = React.useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    postUrl();
+  };
+
+  const resetForm = () => {
+    resetLongUrl();
+    resetShortUrl();
+  };
+
+  const postUrl = async () => {
+    setStatus(() => "PENDING");
+    const res = await fetch("/short", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        longUrl: longUrl,
+        shortUrl: shortUrl,
+      }),
+    });
+    if (res.status === 200) {
+      const result = await res.json();
+      setOrange(() => result.shortUrl);
+      setStatus("SUCCESS");
+      resetForm();
+    } else {
+      const result = await res.json();
+      setStatus(() => "FAIL");
+      setErrorMessage(() => result.message);
+    }
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit} className="has-addons has-addons-centered">
+        <div class="field">
+          <label class="label">Url</label>
+          <div class="control">
+            <input
+              class="input"
+              type="url"
+              placeholder="https://benpaat.xyz"
+              name="url"
+              {...bindLongUrl}
+              required
+            />
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">Short URL</label>
+          <div class="control">
+            <input
+              class="input"
+              type="text"
+              placeholder="12345"
+              name="url"
+              {...bindShortUrl}
+            />
+          </div>
+        </div>
+
+        <div class="field">
+          <div class="control">
+            <button type="submit" value="submit" class="button is-primary">
+              Submit
+            </button>
+          </div>
+        </div>
+        <div className="has-text-centered">
+          {status === "SUCCESS" ? (
+            <FormSuccess url={`${window.location.host}/${orange}`} />
+          ) : null}
+          {status === "FAIL" ? <FormFail message={errorMessage} /> : null}
+        </div>
+      </form>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <section class="section">
+      <UrlForm />
+    </section>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
